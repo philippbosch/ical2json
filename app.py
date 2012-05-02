732d8705111a33c83e75a7e097c9ced0b6ace11e
Philipp Bosch <hello@pb.io>
2012-05-02 16:54:42 +0200

    Activate debug mode.

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def convert_from_url(url):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
