6fd3a665aeca29efaddeab4e3d4a1c18b72a578e
Philipp Bosch <hello@pb.io>
2020-07-29 06:58:29 +0000

    py3 compatibility, update dependencies, fix date formatting

--- app.py
+++ app.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import os
+
 from flask import Flask, abort, jsonify, request
 from icalendar import Calendar
-from urllib import urlopen
+import requests
+
 
 app = Flask(__name__)
 
@@ -18,23 +20,31 @@ def convert_from_url(url):
         abort(404)
     if not url.startswith('http'):
         url = 'http://%s' % url
-    uh = urlopen(url)
-    if uh.getcode() != 200:
-        abort(404)
-    ics = uh.read()
-    uh.close()
+
+    try:
+        r = requests.get(url)
+    except:
+        abort(500)
+    
+    if not r.ok:
+        abort(r.status_code)
+    ics = r.content
     cal = Calendar.from_ical(ics)
     data = {}
     data[cal.name] = dict(cal.items())
 
     for component in cal.subcomponents:
-        if not data[cal.name].has_key(component.name):
+        if not component.name in data[cal.name]:
             data[cal.name][component.name] = []
 
         comp_obj = {}
         for item in component.items():
-            comp_obj[item[0]] = unicode(item[1])
-
+            if hasattr(item[1], 'dt'):
+                val = item[1].dt.isoformat()
+            else:
+                val = item[1].to_ical().decode('utf-8').replace('\\,', ',')
+            comp_obj[item[0]] = val
+    
         data[cal.name][component.name].append(comp_obj)
 
     resp = jsonify(data)
